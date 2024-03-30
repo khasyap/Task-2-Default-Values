1a15b0a79dae149d0896b0cffa6813471a87a411
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2024-03-30 10:53:17 +0530

    Code.cs

---
+++ Code.cs
@@ -0,0 +1,30 @@
+namespace Task2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string x = default;
+            int a = default;
+            float b = default;
+            double c = default;
+            char d = default;
+            decimal e = default;
+            bool f = default;
+            short s = default;
+            long l = default;
+            object o = default;
+            Console.WriteLine("String : ",x);
+            Console.WriteLine("int    : "+a);
+            Console.WriteLine("float  : "+b);
+            Console.WriteLine("double : "+c);
+            Console.WriteLine("char   : "+d);
+            Console.WriteLine("decimal: "+e);
+            Console.WriteLine("float  : ",f);
+            Console.WriteLine("short  : "+s);
+            Console.WriteLine("long   : "+l);
+            Console.WriteLine("object : ",o);
+
+        }
+    }
+}
